1aca7293212eeb0eca3ff38567cf06e35ee975b8
Danil Ziborov <ziborov.daniel@yandex.ru>
2022-12-15 14:11:27 +0300

    feat(popup): make popup logic

---
+++ script.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container')
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_job');
+const closeButton = popup.querySelector('.popup__close-button')
+const editButton = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+function openPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    popup.classList.add('popup_opened');
+
+}
+
+function saveChanges(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+popupContainer.addEventListener('submit', saveChanges);
+closeButton.addEventListener('click', closePopup);
+
+
