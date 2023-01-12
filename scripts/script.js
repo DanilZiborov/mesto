7afa52f743d3a70a3e93442926d6df8ef78c27ae
Danil Ziborov <ziborov.daniel@yandex.ru>
2023-01-12 17:46:41 +0300

    feat(cards): show default cards from array

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,35 @@
+
+//Массив изначальных карточек
+
+const initialCards = [
+  {
+    name: 'Железноводск',
+    link: 'images/zhl.png'
+  },
+  {
+    name: 'Архыз',
+    link: 'images/arh.png'
+  },
+  {
+    name: 'Бермамыт',
+    link: 'images/berm.png'
+  },
+  {
+    name: 'Лермонтов',
+    link: 'images/lerm.png'
+  },
+  {
+    name: 'Новотерский',
+    link: 'images/nvtr.png'
+  },
+  {
+    name: 'Пятигорск',
+    link: 'images/ptg.png'
+  },
+];
+
+//Элементы попапа редкатирования личных данных
+
 const popup = document.querySelector('.popup');
 const popupForm = popup.querySelector('.form')
 const nameInput = popup.querySelector('.popup__input_type_name');
@@ -6,6 +38,15 @@ const closeButton = popup.querySelector('.popup__close-button')
 const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
+
+//Элементы карточек
+
+const cardsSection = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card').content;
+
+
+//Логика открытия попапа редактирования личных данных
+
 
 function openPopup() {
   nameInput.value = profileName.textContent;
@@ -25,8 +66,25 @@ function saveChanges(evt) {
 }
 
 
+// Логика добавления дефолтных карточек
+
+function showInitialCards() {
+  initialCards.forEach(item => {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__image').src = item.link;
+    cardsSection.append(cardElement);
+  });
+};
+
+// Слушатели событий
+
 editButton.addEventListener('click', openPopup);
 popupForm.addEventListener('submit', saveChanges);
 closeButton.addEventListener('click', closePopup);
+
+// Генерим дефолтные карточки
+
+showInitialCards();
 
 
